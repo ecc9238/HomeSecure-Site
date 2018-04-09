6ca078e69b31390472e502a3232c5eef2c88cb04
ecc9238 <echampion11@gmail.com>
2018-04-09 08:57:17 -0500

    Create pan.sh

---
+++ pan.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Edward C. Champion 4/9/2018
+while true; do
+	hor.sh 0
+	sleep 15
+	hor.sh 10
+	hor.sh 20
+	hor.sh 30
+	hor.sh 40
+	hor.sh 50
+	hor.sh 60
+	hor.sh 70
+	hor.sh 80
+	hor.sh 90
+	sleep 15
+	hor.sh 80
+	hor.sh 70
+	hor.sh 60
+	hor.sh 50
+	hor.sh 40
+	hor.sh 30
+	hor.sh 20
+	hor.sh 10
+	sleep 15
+	hor.sh 0
+
+
+done
+#sudo python hor.sh
